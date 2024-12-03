3f86be1feb11fbf906897788cd795ff495ef7def
Amer-Morsy <amermousa341@gmail.com>
2024-12-03 03:28:37 +0300

    clean main

--- routes/web.php
+++ routes/web.php
@@ -1,29 +1,10 @@
 <?php
 
-use App\Http\Controllers\HelloController;
+
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\MathController;
 
 
 Route::get('/', function () {
-    $testUrl = route('test.sum', [1, 2]);
-    dump($testUrl);
     return view('welcome');
 });
 
-Route::get('/hello', [HelloController::class, 'welcome']);
-
-Route::name('test.')->prefix('test')->group(function () {
-    Route::get('/sum/{a}/{b}', function ($a, $b) {
-        return $a + $b;
-    })->name('sum')->whereNumber(['a', 'b']);
-});
-
-Route::controller(MathController::class)->group(function () {
-    Route::get('/sum/{a}/{b}', 'sum')->whereNumber(['a', 'b']);
-    Route::get('/subtract/{a}/{b}', 'subtract')->whereNumber(['a', 'b']);
-});
-
-Route::fallback(function () {
-    return 'fallback';
-});
